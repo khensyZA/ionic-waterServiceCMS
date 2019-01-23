3102e1611c6ae57173599f600b7106a85596d7a0
khensani <khensanijoe@gmail.com>
2019-01-23 10:36:20 +0200

    trying smething out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,22 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+//  import * as firebase from 'firebase';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+
+
+//  var config = {
+//    apiKey: "AIzaSyCDA2SmyMOpqB49eOYtL566O6_QZOQL9zQ",
+//    authDomain: "waterapp-aa3d2.firebaseapp.com",
+//    databaseURL: "https://waterapp-aa3d2.firebaseio.com",
+//    projectId: "waterapp-aa3d2",
+//    storageBucket: "waterapp-aa3d2.appspot.com",
+//    messagingSenderId: "319915755205"
+//  };
+//  firebase.initializeApp(config);
 
 @NgModule({
   declarations: [
